9adf4d56a566920f7e488dad14ec22d3435f4618
Tanyaton Oranrigsupak <tanyatonoran@Tanyatons-MacBook-Air.local>
2024-11-28 16:16:48 +0700

    feat: training new hand model

--- hand-gesture-recognition/function.py
+++ hand-gesture-recognition/function.py
@@ -106,7 +106,9 @@ def logging_csv(number, mode, landmark_list, point_history_list):
     if mode == 0:
         pass
     if mode == 1 and (0 <= number <= 9):
-        csv_path = 'model/keypoint_classifier/keypoint.csv'
+        # csv_path = 'model/keypoint_classifier/keypoint.csv'
+        csv_path = 'model/keypoint_classifier/keypoint_27nov/keypoint2.csv'
+
         with open(csv_path, 'a', newline="") as f:
             writer = csv.writer(f)
             writer.writerow([number, *landmark_list])
